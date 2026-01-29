a38ccafa6b1b8891c5835bec9ead69bcbfbe13ef
Michael Shyam <michaelshyam111@gmail.com>
2026-01-29 20:20:44 +0800

    Level-3: Tony is able to mark and unmark tasks as done and undone respectively

---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "[X]" : "[ ]";
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String toString() {
+        return   getStatusIcon()  + description;
+    }
+
+}
--- src/main/java/Tony.java
+++ src/main/java/Tony.java
@@ -4,7 +4,7 @@ public class Tony {
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("    ____________________________________________________________");
@@ -15,17 +15,35 @@ public class Tony {
         while (true) {
             line = in.nextLine();
             System.out.println("    ____________________________________________________________");
+
             if (line.equals("bye")) {
                 System.out.println("      Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
                 break;
             } else if (line.equals("list")) {
+                //Display all tasks in order
+                System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println("      " + (i + 1) + ". " + tasks[i]);
+                    System.out.println("      " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println("    ____________________________________________________________");
-            } else {
-                tasks[taskCount] = line;
+            } else if (line.startsWith("mark ")) {
+                //mark task as done
+                int taskNumber = Integer.parseInt(line.substring(5)) -1;
+                tasks[taskNumber].markAsDone();
+                System.out.println("       Nice! I've marked this task as done:" );
+                System.out.println("          " + tasks[taskNumber]);
+                System.out.println("    ____________________________________________________________");
+            } else if (line.startsWith("unmark ")) {
+                //mark task as undone
+                int taskNumber = Integer.parseInt(line.substring(7)) -1;
+                tasks[taskNumber].markAsNotDone();
+                System.out.println("       OK, I've marked this task as not done yet:" );
+                System.out.println("          " + tasks[taskNumber]);
+                System.out.println("    ____________________________________________________________");
+            }
+            else {
+                tasks[taskCount] = new Task(line);
                 taskCount++;
                 System.out.println("     added:" + line);
                 System.out.println("    ____________________________________________________________");
