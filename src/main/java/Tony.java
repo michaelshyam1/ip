a86126e5f1f174b3898ae723c7c60f35e83f988e
Michael Shyam <michaelshyam111@gmail.com>
2026-02-12 02:43:11 +0800

    Level-5: Refactored command handling to switch-case and added error handling for incorrect inputs

--- src/main/java/Tony.java
+++ src/main/java/Tony.java
@@ -16,69 +16,94 @@ public class Tony {
             line = in.nextLine();
             System.out.println("    ____________________________________________________________");
 
-            if (line.equals("bye")) {
+            String command = line.split(" ")[0];
+
+            switch (command) {
+            case "bye":
                 System.out.println("      Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
-                break;
-            } else if (line.equals("list")) {
+                return;
+            case "list":
                 //Display all tasks in order
                 System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println("      " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println("    ____________________________________________________________");
-            } else if (line.startsWith("mark ")) {
+                break;
+            case "mark":
                 //mark task as done
-                int taskNumber = Integer.parseInt(line.substring(5)) -1;
-                tasks[taskNumber].markAsDone();
-                System.out.println("       Nice! I've marked this task as done:" );
-                System.out.println("          " + tasks[taskNumber]);
+                int markNumber = Integer.parseInt(line.substring(5)) - 1;
+                tasks[markNumber].markAsDone();
+                System.out.println("       Nice! I've marked this task as done:");
+                System.out.println("          " + tasks[markNumber]);
                 System.out.println("    ____________________________________________________________");
-            } else if (line.startsWith("unmark ")) {
+                break;
+            case "unmark":
                 //mark task as undone
-                int taskNumber = Integer.parseInt(line.substring(7)) -1;
-                tasks[taskNumber].markAsNotDone();
-                System.out.println("       OK, I've marked this task as not done yet:" );
-                System.out.println("          " + tasks[taskNumber]);
+                int unmarkNumber = Integer.parseInt(line.substring(7)) - 1;
+                tasks[unmarkNumber].markAsNotDone();
+                System.out.println("       OK, I've marked this task as not done yet:");
+                System.out.println("          " + tasks[unmarkNumber]);
                 System.out.println("    ____________________________________________________________");
-            } else if (line.startsWith("todo ")) {
-                String description = line.substring(5);
-                tasks[taskCount] = new Todo(description);
+                break;
+
+            case "todo":
+                //handle error
+                if (line.trim().equals("todo")) {
+                    System.out.println("       AHHHH ~ todo cannot be empty!!");
+                    System.out.println("    ____________________________________________________________");
+                    break;
+                }
+                String todoDescription = line.substring(5);
+                tasks[taskCount] = new Todo(todoDescription);
                 taskCount++;
                 System.out.println("       Got it. I've added this task:");
                 System.out.println("          " + tasks[taskCount - 1]);
                 System.out.println("    Now you have " + taskCount + " tasks in the list");
                 System.out.println("    ____________________________________________________________");
-            } else if (line.startsWith("deadline ")) {
-                String[] parts = line.substring(9).split(" /by ");
-                String description = parts[0];
-                String by = parts[1];
-                tasks[taskCount] = new Deadline(description, by);
+                break;
+            case "deadline":
+                if (line.trim().equals("deadline")) {
+                    System.out.println("       AHHHH ~ deadline cannot be empty!!");
+                    System.out.println("    ____________________________________________________________");
+                    break;
+                }
+                String[] deadlineParts = line.substring(9).split(" /by ");
+                String deadlineDescription = deadlineParts[0];
+                String by = deadlineParts[1];
+                tasks[taskCount] = new Deadline(deadlineDescription, by);
                 taskCount++;
                 System.out.println("       Got it. I've added this task:");
                 System.out.println("          " + tasks[taskCount - 1]);
                 System.out.println("    Now you have " + taskCount + " tasks in the list");
                 System.out.println("    ____________________________________________________________");
-            }  else if (line.startsWith("event ")) {
-                String content = line.substring(6);
-                String[] firstSplit = content.split(" /from ");
-                String description = firstSplit[0];
+                break;
+            case "event":
+                //handle error
+                if (line.trim().equals("event")) {
+                    System.out.println("       AHHHH ~ event cannot be empty!!");
+                    System.out.println("    ____________________________________________________________");
+                    break;
+                }
+                String eventContent = line.substring(6);
+                String[] firstSplit = eventContent.split(" /from ");
+                String eventDescription = firstSplit[0];
                 String[] secondSplit = firstSplit[1].split(" /to ");
                 String from = secondSplit[0];
                 String to = secondSplit[1];
 
-                tasks[taskCount] = new Event(description, from, to);
+                tasks[taskCount] = new Event(eventDescription, from, to);
                 taskCount++;
                 System.out.println("       Got it. I've added this task:");
                 System.out.println("          " + tasks[taskCount - 1]);
                 System.out.println("    Now you have " + taskCount + " tasks in the list");
                 System.out.println("    ____________________________________________________________");
-
-            }else {
-                tasks[taskCount] = new Todo(line);
-                taskCount++;
-                System.out.println("     added:" + line);
+                break;
+            default:
+                System.out.println("      ⚠\uFE0F Invalid command! ");
                 System.out.println("    ____________________________________________________________");
+                break;
             }
         }
     }
