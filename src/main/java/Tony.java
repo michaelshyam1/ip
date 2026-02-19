f7c5e5b432f1626ea07d365ef25b0e9b0c1f1017
Michael Shyam <michaelshyam111@gmail.com>
2026-02-19 20:47:02 +0800

    Level-7: Added save and load tasks from file feature to Tony

--- src/main/java/Tony.java
+++ src/main/java/Tony.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Tony {
+    private static ArrayList<Task> tasks = new ArrayList<>();
+    private static Storage storage = new Storage("./data/duke.txt");
+
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+
+        // Load tasks from file at startup
+        tasks = storage.loadTasks();
 
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Tony");
@@ -24,42 +29,39 @@ public class Tony {
                     System.out.println("    ____________________________________________________________");
                     return;
                 case "list":
-                    //Display all tasks in order
                     System.out.println("    Here are the tasks in your list:");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println("      " + (i + 1) + "." + tasks[i]);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("      " + (i + 1) + "." + tasks.get(i));
                     }
                     System.out.println("    ____________________________________________________________");
                     break;
                 case "mark":
-                    //mark task as done
                     int markNumber = Integer.parseInt(line.substring(5)) - 1;
-                    tasks[markNumber].markAsDone();
+                    tasks.get(markNumber).markAsDone();
                     System.out.println("       Nice! I've marked this task as done:");
-                    System.out.println("          " + tasks[markNumber]);
+                    System.out.println("          " + tasks.get(markNumber));
                     System.out.println("    ____________________________________________________________");
+                    storage.saveTasks(tasks);
                     break;
                 case "unmark":
-                    //mark task as undone
                     int unmarkNumber = Integer.parseInt(line.substring(7)) - 1;
-                    tasks[unmarkNumber].markAsNotDone();
+                    tasks.get(unmarkNumber).markAsNotDone();
                     System.out.println("       OK, I've marked this task as not done yet:");
-                    System.out.println("          " + tasks[unmarkNumber]);
+                    System.out.println("          " + tasks.get(unmarkNumber));
                     System.out.println("    ____________________________________________________________");
+                    storage.saveTasks(tasks);
                     break;
-
                 case "todo":
-                    //handle error
                     if (line.trim().equals("todo")) {
                         throw new TonyException("       AHHHH ~ todo cannot be empty!!");
                     }
                     String todoDescription = line.substring(5);
-                    tasks[taskCount] = new Todo(todoDescription);
-                    taskCount++;
+                    tasks.add(new Todo(todoDescription));
                     System.out.println("       Got it. I've added this task:");
-                    System.out.println("          " + tasks[taskCount - 1]);
-                    System.out.println("    Now you have " + taskCount + " tasks in the list");
+                    System.out.println("          " + tasks.get(tasks.size() - 1));
+                    System.out.println("    Now you have " + tasks.size() + " tasks in the list");
                     System.out.println("    ____________________________________________________________");
+                    storage.saveTasks(tasks);
                     break;
                 case "deadline":
                     if (line.trim().equals("deadline")) {
@@ -68,15 +70,14 @@ public class Tony {
                     String[] deadlineParts = line.substring(9).split(" /by ");
                     String deadlineDescription = deadlineParts[0];
                     String by = deadlineParts[1];
-                    tasks[taskCount] = new Deadline(deadlineDescription, by);
-                    taskCount++;
+                    tasks.add(new Deadline(deadlineDescription, by));
                     System.out.println("       Got it. I've added this task:");
-                    System.out.println("          " + tasks[taskCount - 1]);
-                    System.out.println("    Now you have " + taskCount + " tasks in the list");
+                    System.out.println("          " + tasks.get(tasks.size() - 1));
+                    System.out.println("    Now you have " + tasks.size() + " tasks in the list");
                     System.out.println("    ____________________________________________________________");
+                    storage.saveTasks(tasks);
                     break;
                 case "event":
-                    //handle error
                     if (line.trim().equals("event")) {
                         throw new TonyException("       AHHHH ~ event cannot be empty!!");
                     }
@@ -87,21 +88,35 @@ public class Tony {
                     String from = secondSplit[0];
                     String to = secondSplit[1];
 
-                    tasks[taskCount] = new Event(eventDescription, from, to);
-                    taskCount++;
+                    tasks.add(new Event(eventDescription, from, to));
                     System.out.println("       Got it. I've added this task:");
-                    System.out.println("          " + tasks[taskCount - 1]);
-                    System.out.println("    Now you have " + taskCount + " tasks in the list");
+                    System.out.println("          " + tasks.get(tasks.size() - 1));
+                    System.out.println("    Now you have " + tasks.size() + " tasks in the list");
                     System.out.println("    ____________________________________________________________");
+                    storage.saveTasks(tasks);
+                    break;
+                case "delete":
+                    if (line.trim().equals("delete")) {
+                        throw new TonyException("OOPS!!! Please specify which task to delete. Use: delete TASK_NUMBER");
+                    }
+                    int deleteNumber = Integer.parseInt(line.substring(7)) - 1;
+                    if (deleteNumber < 0 || deleteNumber >= tasks.size()) {
+                        throw new TonyException("OOPS!!! Task number is out of range!");
+                    }
+                    Task deletedTask = tasks.remove(deleteNumber);
+                    System.out.println("       Noted. I've removed this task:");
+                    System.out.println("          " + deletedTask);
+                    System.out.println("    Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("    ____________________________________________________________");
+                    storage.saveTasks(tasks);
                     break;
                 default:
-                    throw new TonyException("      ⚠️ Invalid command! ");
+                    throw new TonyException("      ⚠ Invalid command! ");
                 }
             } catch(TonyException e) {
                 System.out.println("      " + e.getMessage());
                 System.out.println("    ____________________________________________________________");
             }
-
         }
     }
 }
